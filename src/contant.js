c395cffd7c2f46f5666388ba14f6055a88ba2e1f
ShahmeerBhola <shahmir.shamim1@gmail.com>
2022-06-04 20:42:39 +0500

    push

---
+++ src/contant.js
@@ -0,0 +1,1 @@
+export const server_address='http://192.168.1.108:5000';
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,27 @@
+import axios from "axios";
 import React from "react";
+import { server_address } from "../contant";
 
 function Login() {
+  const [user,setUser]=React.useState({});
+  const inputHandler=(e)=>{
+    setUser({
+      ...user,
+      [e.target.name]:e.target.value
+    })
+
+  }
+  console.log("user",user);
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    axios.post(`${server_address}/login`, user)
+    .then((res)=>{
+      console.log('res',res);
+    })
+    .catch((error)=>{
+      console.log("error",error)
+    })
+  }
   return (
     <div className="h-full w-full overflow-hidden">
       <div className="">
@@ -9,23 +30,26 @@ function Login() {
             {/* <img src="/img/linktree-logo.png" className="w-[170px] pb-14" /> */}
           </div>
           <div className="border w-[40%] rounded-2xl pb-6 mx-auto">
-            <div className="flex flex-col gap-7 py-7 w-10/12  mx-auto">
+            <form onSubmit={submitHandler} className="flex flex-col gap-7 py-7 w-10/12  mx-auto">
               <h2 className="text-3xl text-center py-4">
                 Log into your account
               </h2>
               <input
                 className="text-base bg-[#f5f6f8] py-3 rounded-lg px-3"
-                placeholder="Username"
+                placeholder="Username" onChange={inputHandler} required 
+                name="username"
               />
               <input
                 className="text-base bg-[#f5f6f8] py-3 rounded-lg px-3"
                 type="password"
                 placeholder="Password"
+                onChange={inputHandler} required
+                name="password"
               />
               <button className="text-[#fff] py-3 font-semibold rounded-xl bg-gradient-to-r from-sky-500 to-indigo-500">
                 Log in
               </button>
-            </div>
+            </form>
           </div>
         </div>
       </div>
